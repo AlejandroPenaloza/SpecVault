85fc7c0e14e0b33d4213bbe148b989a512462772
Alejandro Penaloza <alejandro.penaloza.20@gmail.com>
2026-01-21 22:03:24 -0600

    Added created table trading_cards

--- supabase/dev_log.sql
+++ supabase/dev_log.sql
@@ -14,7 +14,7 @@
   -------------------------
   Alejandro Penaloza
   Created: 2026/01/02
-  Updated: 2026/01/13
+  Updated: 2026/01/21
 */
 -- DO NOT RUN IN PRODUCTION
 
@@ -250,3 +250,19 @@ SELECT
 FROM new_item;
 
 COMMIT;
+
+
+-- create table trading_cards
+CREATE TABLE trading_cards (
+  item_id uuid PRIMARY KEY
+    REFERENCES items(id) ON DELETE CASCADE,
+
+  topic text NOT NULL,                  -- Sport, discipline, or other topic
+  brand text,                           -- Topps, Panini, Upper Deck
+  set_name text,                        -- Set or series name
+  year integer CHECK (year >= 1800),    -- Card issue year
+  card_number text,                     -- Alphanumeric
+  condition text,                       -- NM, EX, PSA 9,...
+  is_autographed boolean DEFAULT false, -- Whether marker autograph on card
+  notes text
+);
